bb330055640bb19c61da7d8f222946e1a19b2890
Felipe Aguayo <felipe.aguayo8977@stu.mtec.edu>
2022-10-14 12:17:00 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white');
+
+$('#nav').css('background-color', '#145');
+
+$('li:contains("even")').css('background-color', '#ccc');
+
+$('li:contains("odd")').css('background-color', '#aaa');
+
+$('#myInput').val('Felipe');
